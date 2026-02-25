1f76ccf248d263995786ff4c9b3bf7b7a693774b
Aws12505 <166876985+Aws12505@users.noreply.github.com>
2026-02-25 10:55:28 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\PublicStoreController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthTokenStoreScopeMiddleware;
 
 /*
 |--------------------------------------------------------------------------
